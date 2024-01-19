5189f7a2249322839fba15f9573aada9c5d3f161
Valeria <16-11233@usb.ve>
2024-01-18 23:28:56 -0400

    responsive design

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import Navbar from "@/components/Navbar"
 import { StarIcon } from "@heroicons/react/16/solid"
 
@@ -5,20 +7,20 @@ export default function Home() {
   return (
     <div className='grid grid-cols-12 gap-4 h-max bg-[#75aadb] relative z-10'>
       <Navbar/>
-      <div className="absolute top-0 left-0 right-0 bottom-0 text-center z-20 text-xl py-40 justify-center">
+      <div className="z-20 absolute top-0 left-0 right-0 bottom-0 text-center text-xl py-40 justify-center">
         <div className="grid justify-items-center mt-4 gap-8">
-          <div className="w-1/4 text-[#FCBF45]">
-            <StarIcon className="w-16 mx-auto"/>
+          <div className="w-1/2 text-[#FCBF45]">
+            <StarIcon className="w-10 sm:w-16 mx-auto"/>
             <div className="flex justify-center">
-              <StarIcon className="w-16 mx-auto mb-5 rotate-45"/>
-              <StarIcon className="w-16 mx-auto mb-5 -rotate-45"/>
+              <StarIcon className="w-10 sm:w-16 mx-auto mb-5 rotate-45"/>
+              <StarIcon className="w-10 sm:w-16 mx-auto mb-5 -rotate-45"/>
             </div>
           </div>
           <div className="align-center font-bold text-white">
-            <h1 className="text-5xl uppercase">Intercambia tus barajitas</h1>
-            <h2 className="text-5xl">conoce a otros fanáticos</h2>
+            <h1 className="text-4xl sm:text-5xl uppercase">Intercambia tus barajitas</h1>
+            <h2 className="text-4xl sm:text-5xl">conoce a otros fanáticos</h2>
           </div>
-          <button className="bg-[#FCBF45] py-3 px-4 font-semibold rounded-xl">
+          <button className="bg-[#FCBF45] py-3 px-4 font-semibold rounded-xl w-1/8">
             <a href="/#">Regístrate</a>
           </button>
         </div>
--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -1,24 +1,45 @@
+import { useState } from "react"
 import { BookOpenIcon } from "@heroicons/react/16/solid"
+import { Bars3Icon } from "@heroicons/react/16/solid"
 
-const Navbar = () => {
+export default function Navbar() {
+  const [open, setOpen] = useState(false)
   return (
-    <nav className="bg-white fixed w-full z-20 top-0 start-0 bg-opacity-50">
-      <div className="mx-auto max-w-screen-xl px-10 py-2">
+    <nav className="bg-transparent fixed w-full z-30 top-0 start-0">
+      <div className="px-10 py-2 bg-white bg-opacity-50">
         <div className="flex justify-between py-2 items-center">
-          <a href="#" className="flex">
+          {/* Title */}
+          <a href="/" className="flex">
             <BookOpenIcon className="w-8"/>
             <div className="font-bold text-2xl">Barajitas</div>
           </a>
-          <div className="flex items-center space-x-5 font-medium">
-            <a href="/" className="mr-4">FAQ</a>
+          {/* Links */}
+          <div className="hidden md:flex items-center space-x-5 font-medium">
+            <a href="#" className="mr-4">FAQ</a>
             <a href="#" className="mr-4">Contacto</a>
             <a href="#" className="mr-4">Álbumes Disponibles</a>
           </div>
-          <a className="font-bold text-xl" href="#">Entrar</a>
+          {/* Login */}
+          <div className="flex">
+          <a href="#" className="hidden md:flex font-bold text-xl">Entrar</a>
+
+          {/* Mobile */}
+          <button onClick={() => setOpen(!open)}>
+            <Bars3Icon className="sm:hidden md: w-8"/>
+          </button>
+          </div>
         </div>
       </div>
+      { open &&
+      <div className="flex justify-end p-2 md:hidden">
+        <div className="bg-white px-2 py-2 bg-opacity-50 rounded-xl text-right">
+          <a href="#" className="block py-2 hover:bg-white hover:bg-opacity-30 px-4 rounded-xl">FAQ</a>
+          <a href="#" className="block py-2 hover:bg-white hover:bg-opacity-30 px-4 rounded-xl">Contacto</a>
+          <a href="#" className="block py-2 hover:bg-white hover:bg-opacity-30 px-4 rounded-xl">Álbumes<br/>Disponibles</a>
+          <a href="#" className="block py-2 font-bold text-xl px-4 rounded-xl hover:bg-white hover:bg-opacity-50">Entrar</a>
+        </div>
+      </div>
+      }
     </nav>
   )
 }
-
-export default Navbar
