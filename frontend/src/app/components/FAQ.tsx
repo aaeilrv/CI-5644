a396940b49d713bdd12e6c97bb141e59def24fcf
Valeria <16-11233@usb.ve>
2024-01-21 20:35:20 -0400

    doing FAQ

--- frontend/src/app/components/FAQ.tsx
+++ frontend/src/app/components/FAQ.tsx
@@ -1,10 +1,67 @@
 "use client";
+import Image from "next/image";
+
+const preguntas = [
+  {
+    pregunta: "¿Lorem ipsum dolor?",
+    respuesta: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio."
+  },
+  {
+    pregunta: "¿Lorem ipsum dolor sit amet?",
+    respuesta: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet."
+  },
+  {
+    pregunta: "¿Lorem ipsum dolor sit amet?",
+    respuesta: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero."
+  },
+  {
+    pregunta: "¿Lorem ipsum dolor?",
+    respuesta: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio."
+  },
+  {
+    pregunta: "¿Lorem ipsum dolor sit amet?",
+    respuesta: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero. Sed cursus ante dapibus diam. Sed nisi. Nulla quis sem at nibh elementum imperdiet."
+  },
+  {
+    pregunta: "¿Lorem ipsum dolor sit amet?",
+    respuesta: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Integer nec odio. Praesent libero."
+  },
+]
 
 export default function FAQ() {
   return (
-    <div className='w-full h-screen flex items-center justify-center bg-gradient-to-bl from-[#DA291C] from-50% to-[#046A38] to-50%'>
-      <div className="font-bold text-white">
-        <h1 className="text-4xl sm:text-5xl uppercase">FAQ</h1>
+    <div className='w-full h-screen flex items-center justify-center bg-gradient-to-bl from-[#DA291C] from-50% to-[#046A38] to-50% space-x-20'>
+      <div className="font-bold text-white space-y-4 w-5/6 h-5/6 md:h-1/2 p-2 md:p-4 md:w-1/2 overflow-scroll no-scrollbar border-4 rounded-lg">
+        <h1 className="text-4xl sm:text-5xl uppercase">F.A.Q</h1>
+        <div className="space-y-10">
+        {
+          preguntas.map((pregunta, index) => (
+            <div key={index}>
+              <h3 className="text-2xl">
+                {pregunta.pregunta}
+              </h3>
+              <p className="text-white text-wrap">
+                {pregunta.respuesta}
+              </p>
+            </div>
+          ))
+        }
+        </div>
+      </div>
+
+      {/* Barajitas */}
+      <div className="hidden md:flex">
+        <div className="relative">
+          <div className="absolute p-2 bg-white rounded-lg shadow z-50">
+            <Image src="/static/images/cristiano_barajita.png" width={200} height={40} alt={"Cristiano"}/>
+          </div>
+          <div className="absolute top-0 left-0 bg-white rounded-lg p-2 ml-16 z-40" style={{ transform: 'rotate(20deg)' }}>
+            <Image src="/static/images/messi_barajita.png" width={200} height={40} alt={"Messi"}/>
+          </div>
+          <div className="top-0 left-0 bg-white rounded-lg p-2 ml-24 z-30" style={{ transform: 'rotate(45deg)' }}>
+            <Image src="/static/images/mbappe_barajita.png" width={200} height={40} alt={"Mbappe"}/>
+          </div>
+        </div>
       </div>
     </div>
   )
--- frontend/src/app/components/Testimonials.tsx
+++ frontend/src/app/components/Testimonials.tsx
@@ -1,11 +1,51 @@
 "use client";
+import Image from "next/image";
+
 
 export default function Testimonials() {
   return (
-    <div className='bg-black w-full h-screen flex items-center justify-center'>
-      <div className="font-bold text-white">
-        <h1 className="text-4xl sm:text-5xl uppercase">TESTIMONIALS</h1>
+    <div className='bg-[#202437] w-full h-screen flex items-center justify-center space-x-10 overflow-hidden'>
+
+      <div className="grid grid-cols-1 justify-items-center">
+        <div className="text-white">
+          <h1 className="font-bold text-4xl sm:text-5xl uppercase">Testimonios</h1>
+        </div>
+
+        {/* Comentarios */}
+        <div className="grid grid-col-1 md:flex space-x-10">
+          <div className="block max-w-sm p-6 border-8 border-[#ce1939] rounded-lg mt-10">
+            <h5 className="mb-2 text-2xl font-bold italic text-white">"Bslablabla"</h5>
+            <p className="font-normal text-white text-left">-Pepito</p>
+          </div>
+
+          <div className="block max-w-sm p-6 border-8 border-[#ce1939] rounded-lg mt-10">
+            <h5 className="mb-2 text-2xl font-bold italic text-white">"Blablabla"</h5>
+            <p className="font-normal text-white text-left">-Pepito</p>
+          </div>
+
+          <div className="block max-w-sm p-6 border-8 border-[#ce1939] rounded-lg mt-10">
+            <h5 className="mb-2 text-2xl font-bold italic text-white">"Blablabla"</h5>
+            <p className="font-normal text-white text-left">-Pepito</p>
+          </div>
+        </div>
       </div>
+
+      {/* Barajitas */}
+      {/*
+      <div className="hidden md:flex">
+        <div className="relative">
+          <div className="absolute p-2 bg-white rounded-lg shadow z-50">
+            <Image src="/static/images/cristiano_barajita.png" width={200} height={40} alt={"Cristiano"}/>
+          </div>
+          <div className="absolute top-0 left-0 bg-white rounded-lg p-2 ml-16 z-40" style={{ transform: 'rotate(20deg)' }}>
+            <Image src="/static/images/messi_barajita.png" width={200} height={40} alt={"Messi"}/>
+          </div>
+          <div className="top-0 left-0 bg-white rounded-lg p-2 ml-24 z-30" style={{ transform: 'rotate(45deg)' }}>
+            <Image src="/static/images/mbappe_barajita.png" width={200} height={40} alt={"Mbappe"}/>
+          </div>
+        </div>
+      </div>
+      */}
     </div>
   )
 }
