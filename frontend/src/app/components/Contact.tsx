ea3435024fbbc4ad21e134244cad2fbc1540e769
Valeria <16-11233@usb.ve>
2024-01-22 01:03:45 -0400

    listo el landing finalmente?

--- frontend/src/app/components/Contact.tsx
+++ frontend/src/app/components/Contact.tsx
@@ -2,12 +2,12 @@
 
 export default function Contact() {
   return (
-    <div className='bg-[#600f22] w-full h-screen flex items-center justify-center'>
+    <div className='w-full h-screen flex items-center justify-center bg-gradient-to-bl from-[#600f22] from-50% to-[#540a1b] to-50%'>
       <div className="grid grid-cols-1 font-bold text-[#9e7c27] place-items-center px-4">
         <div className="flex justify-between py-2 mx-20 space-x-20">
-          <a href="" className="text-xl">IG</a>
-          <a href="" className="text-xl">TW</a>
-          <a href="" className="text-xl">GH</a>
+          <a href="#" className="text-xl">IG</a>
+          <a href="#" className="text-xl">TW</a>
+          <a href="https://github.com/aaeilrv/CI5644-Barajitas" className="text-xl">GH</a>
         </div>
         <h2 className="text-xl font-normal text-center">CI5644: Herramientas de Programación</h2>
         <h2 className="text-xl font-normal">2024</h2>
