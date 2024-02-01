328d33d6b2b332f2ce52503cf27dbb7bc9de35cf
Santiago Finamore <18-10125@usb.ve>
2024-02-01 13:11:07 -0400

    Agregado DDL de tablas a esquema de flyway

--- backend/src/main/resources/db.migration/v1_0_create_initial_tables.sql
+++ backend/src/main/resources/db.migration/v1_0_create_initial_tables.sql
@@ -6,5 +6,48 @@ CREATE TABLE member (
     birthday DATE NOT NULL,
     username VARCHAR(20) NOT NULL UNIQUE
 );
-INSERT INTO member(id, name, email, password, birthday, username)
-VALUES(1, 'pedro perez', 'pedroperez@test.com', 'password', '02/06/1999', 'pedrox')
+
+CREATE TABLE card(
+    id SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    player_position VARCHAR(30) NOT NULL,
+    player_number VARCHAR(2) NOT NULL,
+    photo VARCHAR(100) NOT NULL
+);
+
+CREATE TABLE purchase(
+    id SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    user_id INTEGER NOT NULL REFERENCES member(id),
+    price INTEGER NOT NULL CHECK(price > 0)
+    --package_id INTEGER NOT NULL REFERENCES package(id)
+    --UNIQUE(user_id, package_id)
+);
+
+CREATE TABLE payment_information(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES member(id),
+    expiration_date DATE NOT NULL,
+    card_number VARCHAR(16) NOT NULL UNIQUE,
+    security_code VARCHAR(3) NOT NULL,
+    cardholder_name VARCHAR(20) NOT NULL,
+    bank VARCHAR(20) NOT NULL
+);
+
+CREATE TABLE exchange(
+    id SERIAL PRIMARY KEY,
+    date DATE NOT NULL,
+    owner_id INTEGER NOT NULL REFERENCES member(id),
+    card_id INTEGER NOT NULL REFERENCES card(id),
+    receiver_id INTEGER NOT NULL REFERENCES member(id),
+    number_of_cards_traded INTEGER NOT NULL CHECK(number_of_cards_traded > 0)
+    --UNIQUE(owner_id, card_id, receiver_id)
+);
+
+CREATE TABLE ownership(
+    id SERIAL PRIMARY KEY,
+    user_id INTEGER NOT NULL REFERENCES member(id),
+    card_id INTEGER NOT NULL REFERENCES card(id),
+    number_of_cards_owned INTEGER NOT NULL CHECK(number_of_cards_owned > 0),
+    UNIQUE(user_id, card_id)
+);
