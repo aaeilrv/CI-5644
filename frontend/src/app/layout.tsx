7fe7e4dafec90fabf2f2878879c68967894d755f
Carlo Herrera <18-10451@usb.ve>
2024-02-03 23:17:12 -0400

    Config. auth0 and login page

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import '../styles/globals.css'
+import React from 'react';
+import { UserProvider } from '@auth0/nextjs-auth0/client';
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +21,10 @@ export default function RootLayout({
       <body className={inter.className}>
         {children}
       </body>
+
+      <UserProvider>
+       <body>{children}</body>
+      </UserProvider>
     </html>
   )
 }
