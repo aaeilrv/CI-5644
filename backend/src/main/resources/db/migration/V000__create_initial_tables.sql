0e9512c2917efa0534b1b9624d821623c79f65c9
Santiago Finamore <18-10125@usb.ve>
2024-02-16 20:19:05 -0400

    DDL - corregido modelo. nombres mas entendibles y corregida definicion de intercambio

--- backend/src/main/resources/db/migration/V000__create_initial_tables.sql
+++ backend/src/main/resources/db/migration/V000__create_initial_tables.sql
@@ -1,3 +1,11 @@
+CREATE TYPE EXCHANGE_REQUEST_STATUS AS ENUM ('pending', 'accepted', 'rejected');
+CREATE TYPE CREDIT_CARD_TYPE AS ENUM ('visa', 'mastercard');
+CREATE TYPE FIELD_POSITION AS ENUM ('goalkeeper',
+                                    'midfield',
+                                    'defense',
+                                    'forward');
+
+
 CREATE TABLE IF NOT EXISTS users (
     id BIGSERIAL PRIMARY KEY,
     first_name VARCHAR(100) NOT NULL,
@@ -11,38 +19,41 @@ CREATE TABLE IF NOT EXISTS users (
 CREATE TABLE IF NOT EXISTS card(
     id BIGSERIAL PRIMARY KEY,
     name VARCHAR(50) NOT NULL,
-    player_position VARCHAR(30) NOT NULL,
-    player_number VARCHAR(2) NOT NULL,
-    photo VARCHAR(100) NOT NULL
+    player_position FIELD_POSITION NOT NULL,
+    player_number SMALLINT NOT NULL,
+    photo VARCHAR(255) NOT NULL
 );
 
-CREATE TABLE IF NOT EXISTS payment_information(
+CREATE TABLE IF NOT EXISTS credit_card(
     id BIGSERIAL PRIMARY KEY,
     user_id BIGINT NOT NULL REFERENCES users(id),
-    expiration_date VARCHAR(8) NOT NULL,
+    expiration_date DATE NOT NULL,
     card_number VARCHAR(16) NOT NULL UNIQUE,
-    security_code VARCHAR(3) NOT NULL,
     cardholder_name VARCHAR(20) NOT NULL,
     bank VARCHAR(20) NOT NULL
+    card_type CREDIT_CARD_TYPE NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS purchase(
     id BIGSERIAL PRIMARY KEY,
-    purchase_date DATE NOT NULL,
+    purchase_timestamp TIMESTAMP NOT NULL,
     user_id BIGINT NOT NULL REFERENCES users(id),
-    price NUMERIC NOT NULL CHECK(price > 0),
-    payment_id BIGINT NOT NULL REFERENCES payment_information(id)
-    --package_id INTEGER NOT NULL REFERENCES package(id)
-    --UNIQUE(user_id, package_id)
+    packets_purchased INT NOT NULL CHECK(packets_purchased > 0),
+    base_amount NUMERIC GENERATED ALWAYS AS (packets_purchased*5) NOT NULL, --Precio hipotetico de $5 por paquete
+    tax_amount NUMERIC GENERATED ALWAYS AS (base_amount*1.12) NOT NULL, --considerando solo IVA por poner ejemplo
+    total_amount NUMERIC GENERATED ALWAYS AS (base_amount + tax_amount) NOT NULL,
+    credit_card_id BIGINT NOT NULL REFERENCES credit_card(id)
 );
 
-CREATE TABLE IF NOT EXISTS exchange(
+CREATE TABLE IF NOT EXISTS exchange_request(
     id BIGSERIAL PRIMARY KEY,
-    date DATE NOT NULL,
-    owner_id BIGINT NOT NULL REFERENCES users(id),
-    card_id BIGINT NOT NULL REFERENCES card(id),
-    receiver_id BIGINT NOT NULL REFERENCES users(id),
-    number_of_cards_traded INTEGER NOT NULL CHECK(number_of_cards_traded > 0)
+    requester_id BIGINT NOT NULL REFERENCES users(id),
+    recipient_id BIGINT NOT NULL REFERENCES users(id),
+    offered_card_id BIGINT NOT NULL REFERENCES card(id),
+    offered_card_amount INT NOT NULL CHECK(number_of_offered_card > 0),
+    requested_card_id BIGINT NOT NULL REFERENCES card(id),
+    requested_card_amount INT NOT NULL CHECK(number_of_requested_card > 0),
+    request_status EXCHANGE_REQUEST_STATUS NOT NULL
 );
 
 CREATE TABLE IF NOT EXISTS ownership(
