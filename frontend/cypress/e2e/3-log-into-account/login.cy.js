7d7cb46b17ec46cb2656cfacfc499de1f5b30b1f
Santiago Finamore <18-10125@usb.ve>
2024-03-08 11:52:51 -0400

    sprint4/testing - Limpieza de test e2e de login

--- frontend/cypress/e2e/3-log-into-account/login.cy.js
+++ frontend/cypress/e2e/3-log-into-account/login.cy.js
@@ -6,19 +6,15 @@ describe('Login with Credentials', () => {
     })
 
     it('Acess Auth0 login page', () => {
-        let auth0URL;
-        let albumURL
         cy.get('#home')
         cy.contains('Regístrate').click()
 
-        cy.wait(1000)
         cy.origin('https://dev-crxb1uekqycez2b5.us.auth0.com/login', () => {
             cy.get('#1-email').type('finamore03@protonmail.com')
             cy.get('#1-password').type('Santoigo16*')
             cy.get('#1-submit').click()
         } )
 
-        cy.wait(2000)
         cy.url().as('albumURL')
         cy.get('@albumURL').should('include', 'http://localhost:3000/album')
     })
